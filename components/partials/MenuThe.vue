b2c1124e7745c383229b10b9bd5878ab1d562970
Dario Tecchia <dariotecchia@gmail.com>
2020-10-14 10:46:29 +0200

    added the menu page

---
+++ components/partials/MenuThe.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="~/assets/menu/the/the-1.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-2.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-3.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-4.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-5.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-6.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-7.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-8.png" alt="" srcset="">
+    <img src="~/assets/menu/the/the-9.png" alt="" srcset="">
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+img {
+  width: 90%;
+}
+</style>
---
+++ pages/the.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="caffetteria">
+    <MenuThe />
+  </div>
+</template>
+
+<script>
+import MenuThe from '~/components/partials/MenuThe.vue'
+
+export default {
+  head: {
+    title: 'Madamé - Caffetteria'
+  },
+  components: {
+    MenuThe
+  }
+}
+</script>
+
+<style>
+.caffetteria {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  text-align: center;
+}
+
+.caffetteria h1 {
+  align-self: center;
+}
+</style>
