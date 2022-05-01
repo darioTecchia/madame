ca319bf1cba3a95cd4d8ac3f8419b8c282a6672a
Dario Tecchia <dariotecchia@gmail.com>
2022-05-01 17:59:13 +0200

    add admin redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://airtable.com/appWGMpWfG81Mwqss/',
+        permanent: false,
+        basePath: false
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
